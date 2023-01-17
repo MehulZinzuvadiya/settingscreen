d27991c61297a0dd1951b51633af88ca1f9ef668
mohitbaraiyaa <mohitbaraiya04@gmail.com>
2023-01-16 19:24:58 -0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'homePage.dart';
+import 'search.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
-      '/': (context) => HomePage(),
+      '/': (context) => Search(),
     },
   ));
 }
---
+++ lib/search.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text("Search"),
+        leading: Icon(Icons.arrow_back),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        actions: [
+          Icon(Icons.search),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(Icons.more_vert),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.search,
+              size: 100,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "No Result",
+              style: TextStyle(
+                  fontSize: 22, letterSpacing: 2, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Try a more general keyword",
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
