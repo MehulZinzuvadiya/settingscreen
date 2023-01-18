d4101618e0ee739dae81e7fcab17922d1610f69c
mohitbaraiyaa <mohitbaraiya04@gmail.com>
2023-01-18 11:48:34 -0800

    4

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
-      '/': (context) => login2(),
+      '/': (context) => Product(),
     },
   ));
 }
--- lib/product.dart
+++ lib/product.dart
@@ -10,29 +10,24 @@ class Product extends StatefulWidget {
 class _ProductState extends State<Product> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/bulb.jpg"),
-            fit: BoxFit.cover,
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/wood.jpeg"),
+          fit: BoxFit.cover,
         ),
-        child: null /* add child content here */,
       ),
-      // appBar: AppBar(
-      //   title: Text("Search"),
-      //   leading: Icon(Icons.arrow_back),
-      //   backgroundColor: Colors.black,
-      //   elevation: 0,
-      //   actions: [
-      //     Icon(Icons.search),
-      //     SizedBox(
-      //       width: 20,
-      //     ),
-      //     Icon(Icons.more_vert),
-      //   ],
-      // ),
-    ));
+      child: Column(
+        children: [
+          Text(
+            "Hi,Mehul Kumar",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          Text(
+            "welcome to the Flutter UIKit",
+          )
+        ],
+      ),
+    );
   }
 }
